b7ac40e95193be24e0c1c3b3e31ef5dfd136458e
Bradley Schaefer <bradley.schaefer@gmail.com>
2013-08-19 17:54:25 -0500

    Extract Rails extension to have matcher
    https://github.com/rspec/rspec-rails/issues/808

--- lib/rspec/collection_matchers.rb
+++ lib/rspec/collection_matchers.rb
@@ -1,3 +1,4 @@
 require 'rspec/collection_matchers/version'
 require 'rspec/collection_matchers/dsl'
 require 'rspec/collection_matchers/have'
+require 'rspec/collection_matchers/rails' if defined? Rails
